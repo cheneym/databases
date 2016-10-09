ca2d9b6a711ee797395bdd63147dacec8e37988c
cheneym <cheneymao94@gmail.com>
2016-10-08 17:14:51 -0700

    Sequelize model message post function

--- server/db/index.js
+++ server/db/index.js
@@ -1,13 +1,33 @@
-var mysql = require('promise-mysql');
-
+// var mysql = require('promise-mysql');
+var Promise = require('bluebird');
 // Create a database connection and export it from this file.
 // You will need to connect with the user "root", no password,
 // and to the database "chat".
-exports.initialize = mysql.createConnection({
-  user: 'root',
-  password: 'yes',
-  database: 'chat'
-});
+// exports.initialize = mysql.createConnection({
+//   user: 'root',
+//   password: 'yes',
+//   database: 'chat'
+// });
 
 // connect = dbConnection.connect();
 // exports.end = dbConnection.end();
+
+var Sequelize = require('sequelize');
+var db = new Sequelize('chatbox', 'root', 'yes');
+
+exports.User = db.define('User', {
+  name: Sequelize.STRING
+});
+
+exports.Room = db.define('Room', {
+  name: Sequelize.STRING
+});
+
+exports.Message = db.define('Message', {
+  text: Sequelize.STRING,
+});
+
+exports.Message.belongsTo(exports.User);
+exports.Message.belongsTo(exports.Room);
+
+// db.sync();
